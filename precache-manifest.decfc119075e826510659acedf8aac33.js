8acc83915d5d462bd371df42260c29c98ae29274
herkoss <dmytroherko@icloud.com>
2021-06-09 12:09:07 +0300

    Updates

--- precache-manifest.decfc119075e826510659acedf8aac33.js
+++ precache-manifest.decfc119075e826510659acedf8aac33.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "150988a1aaa1848a6a2eb8f1d8cd7722",
+    "revision": "daa1aeaa64bdb600b0975fbb662ff93e",
     "url": "/react_people-table-basics/index.html"
   },
   {
-    "revision": "4bff3af48b7ae2595832",
+    "revision": "ffdf46dabcc80e2ebb22",
     "url": "/react_people-table-basics/static/css/main.3908c959.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react_people-table-basics/static/js/2.7916e68e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4bff3af48b7ae2595832",
-    "url": "/react_people-table-basics/static/js/main.3259f7c0.chunk.js"
+    "revision": "ffdf46dabcc80e2ebb22",
+    "url": "/react_people-table-basics/static/js/main.a411d2ec.chunk.js"
   },
   {
     "revision": "30d22e43293fe0ee117b",
